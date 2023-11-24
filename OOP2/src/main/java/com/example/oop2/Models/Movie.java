34c66f09fb937ac51a50b1f16173136b4866b410
Colin Madore <le2287480@crc-lennox.qc.ca>
2023-11-24 14:56:32 -0500

    Added attributes to classes

--- OOP2/src/main/java/com/example/oop2/Models/Movie.java
+++ OOP2/src/main/java/com/example/oop2/Models/Movie.java
@@ -1,4 +1,27 @@
 package com.example.oop2.Models;
 
 public class Movie {
+    private String aTitle;
+    private String aGenre;
+
+    public Movie(String aTitle, String aGenre) {
+        this.aTitle = aTitle;
+        this.aGenre = aGenre;
+    }
+
+    public void setTitle(String aTitle) {
+        this.aTitle = aTitle;
+    }
+
+    public void setGenre(String aGenre) {
+        this.aGenre = aGenre;
+    }
+
+    public String getTitle() {
+        return aTitle;
+    }
+
+    public String getGenre() {
+        return aGenre;
+    }
 }
--- OOP2/src/main/java/com/example/oop2/Models/Showtime.java
+++ OOP2/src/main/java/com/example/oop2/Models/Showtime.java
@@ -1,4 +1,40 @@
 package com.example.oop2.Models;
 
+import java.time.LocalDateTime;
+
 public class Showtime {
+    private LocalDateTime aTime;
+    private Movie aMovie;
+
+    public Showtime(LocalDateTime aTime, Movie aMovie, Showtime aShowtime) {
+        this.aTime = aTime;
+        this.aMovie = aMovie;
+        this.aShowtime = aShowtime;
+    }
+
+    public void setTime(LocalDateTime aTime) {
+        this.aTime = aTime;
+    }
+
+    public void setMovie(Movie aMovie) {
+        this.aMovie = aMovie;
+    }
+
+    public void setShowtime(Showtime aShowtime) {
+        this.aShowtime = aShowtime;
+    }
+
+    public LocalDateTime getTime() {
+        return aTime;
+    }
+
+    public Movie getMovie() {
+        return aMovie;
+    }
+
+    public Showtime getShowtime() {
+        return aShowtime;
+    }
+
+    private Showtime aShowtime;
 }
