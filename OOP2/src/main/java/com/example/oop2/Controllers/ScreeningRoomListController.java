54f03cede0dfe127b23ad9b92e79017194cadfd2
Willuku <le1952006@crc-lennox.qc.ca>
2023-12-02 16:56:46 -0500

    Started ScreeningRoomListController functionality, delete button works

--- OOP2/src/main/java/com/example/oop2/Controllers/ScreeningRoomListController.java
+++ OOP2/src/main/java/com/example/oop2/Controllers/ScreeningRoomListController.java
@@ -1,14 +1,24 @@
 package com.example.oop2.Controllers;
 
 import com.example.oop2.Models.ScreeningRoom;
+import com.example.oop2.Models.ScreeningRoomList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class ScreeningRoomListController {
     @FXML
-    private ListView<ScreeningRoom> screeningRoomListListView;
-
-
+    private ListView<ScreeningRoom> screeningRoomListView;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private void initialize() {
+        screeningRoomListView.setItems(FXCollections.observableArrayList(ScreeningRoomList.getScreeningRoomList()));
+    }
 
     @FXML
     private void onUpdateButtonClick(){
@@ -24,7 +34,13 @@ public class ScreeningRoomListController {
 
     @FXML
     private void onDeleteButtonClick(){
-        System.out.println("onDeleteButtonClick");
-
+        ScreeningRoom selectedRoom = screeningRoomListView.getSelectionModel().getSelectedItem();
+        ScreeningRoomList.removeScreeningList(selectedRoom);
+        initialize();
+    }
+    @FXML
+    public void onCloseButtonClick() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 }
