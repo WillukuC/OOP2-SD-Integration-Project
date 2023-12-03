e86f25d8e72b74fd1bec56628b1028513c32b5e9
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2023-12-02 21:03:41 -0500

    Added documentation to SceneHelper utility class

--- OOP2/src/main/java/com/example/oop2/Models/SceneHelper.java
+++ OOP2/src/main/java/com/example/oop2/Models/SceneHelper.java
@@ -4,18 +4,25 @@ import com.example.oop2.MovieTheatreApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.EventObject;
 
-
+/**
+ * Utility class for managing scenes
+ * You can use changeScene and closeWindow to manage scenes
+ */
 public class SceneHelper {
     private static final SceneHelper sh = new SceneHelper();
 
+    /**
+     * changeScene changes to the desired scene by passing the destination, an ActionEvent and a title
+     * @param destination String holding the destination view
+     * @param pEvent Determines the source of the event
+     * @param title String that holds the title of the scene
+     * */
     public static void changeScene(String destination, ActionEvent pEvent, String title) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MovieTheatreApplication.class.getResource(destination));
         Scene nextScene = new Scene(fxmlLoader.load());
@@ -27,6 +34,10 @@ public class SceneHelper {
         nextStage.showAndWait();
     }
 
+    /**
+     * closeWindow closes the current window using a node to get the current scene
+     * @param node Gets a node from the scene to then close the scene the node is from
+     * */
     public static void closeWindow(Node node) {
         Stage stage = (Stage) node.getScene().getWindow();
         stage.close();
