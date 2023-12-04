9e394499a1d3d1485788f831a999b90308b61f2f
Colin Madore <le2287480@crc-lennox.qc.ca>
2023-12-04 11:29:50 -0500

    created the functionality

--- OOP2/src/main/java/com/example/oop2/Controllers/TicketSalesListController.java
+++ OOP2/src/main/java/com/example/oop2/Controllers/TicketSalesListController.java
@@ -2,12 +2,23 @@ package com.example.oop2.Controllers;
 
 import com.example.oop2.Models.SceneHelper;
 import com.example.oop2.Models.Ticket;
+import com.example.oop2.Models.TicketList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class TicketSalesListController {
     @FXML
-    private ListView<Ticket> ticketSalesListView;
+    private ListView<Ticket> ticketSalesListView = new ListView<>();
+
+    @FXML
+    private void initialize() {
+        ObservableList<Ticket> itemsList = FXCollections.observableArrayList
+                (TicketList.getTicketList());
+        ticketSalesListView.setItems(itemsList);
+    }
+
 
     @FXML
     private void onExitButtonClick(){
