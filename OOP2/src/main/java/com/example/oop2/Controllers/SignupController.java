5dad91e2305686ae940d1d8b86180c7ed6585034
Willuku <le1952006@crc-lennox.qc.ca>
2023-12-06 01:16:47 -0500

    SignupController now adds a new user, + JavaDoc comments

--- OOP2/src/main/java/com/example/oop2/Controllers/SignupController.java
+++ OOP2/src/main/java/com/example/oop2/Controllers/SignupController.java
@@ -1,11 +1,16 @@
 package com.example.oop2.Controllers;
 
 import com.example.oop2.Models.SceneHelper;
-import javafx.event.ActionEvent;
+import com.example.oop2.Models.User;
+import com.example.oop2.Models.UserList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import java.io.IOException;
+import java.time.LocalDateTime;
 
+/**
+ * Allows a new user to sign up for the application.
+ */
 public class SignupController {
     @FXML
     private TextField emailTextField;
@@ -14,9 +19,35 @@ public class SignupController {
     @FXML
     private TextField passwordTextField;
 
+    /**
+     * Checks if the email, username and password are valid.
+     * Creates new user if yes, closing the window.
+     */
     @FXML
-    private void onRegisterButtonClick(ActionEvent actionEvent) throws IOException {
-        System.out.println("onRegisterButtonClick");
-        SceneHelper.closeWindow(passwordTextField);
+    private void onRegisterButtonClick() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        try {
+            if (emailTextField.getText().trim().isEmpty() || !emailTextField.getText().contains("@") || !emailTextField.getText().contains(".")) {
+                alert.setContentText("Please enter a valid email address.");
+                alert.showAndWait();
+            } else if (nameTextField.getText().trim().isEmpty()) {
+                alert.setContentText("Please enter a username.");
+                alert.showAndWait();
+            } else if (passwordTextField.getText().trim().isEmpty()) {
+                alert.setContentText("Please enter a password.");
+            } else {
+                int newUserID = UserList.getUserList().size();
+                String newUsername = nameTextField.getText();
+                String newUserEmail = emailTextField.getText();
+                LocalDateTime accountCreationTime = LocalDateTime.now();
+                String newUserPassword = passwordTextField.getText();
+
+                UserList.addUser(new User(newUserID, newUsername, newUserEmail, accountCreationTime, newUserPassword, false));
+
+                SceneHelper.closeWindow(passwordTextField);
+            }
+        } catch (Exception e) {
+            alert.setContentText("Unexpected error.");
+        }
     }
 }
